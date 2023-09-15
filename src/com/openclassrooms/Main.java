7097a1b86ddb33d58395d6abfecae4c19d93b157
Sylakh <sylvainsds@gmail.com>
2023-09-15 17:57:33 +0200

    mise en place de la fonction main

--- src/com/openclassrooms/Main.java
+++ src/com/openclassrooms/Main.java
@@ -6,28 +6,39 @@ import com.openclassrooms.store.Screen;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Inventory POC");
+	public static void main(String[] args) {
+    	
 
-        Mouse m = new Mouse("DELL", 20.0);
-        Screen s = new Screen("Samsung", 150.0, "1920x1080");
+    	Mouse dellMouse = new Mouse(Brand.DELL, 20.0);
+        Screen samsungScreen = new Screen(Brand.SAMSUNG, 150.0, "1920x1080");
 
         Inventory inventory = new Inventory();
-        inventory.addScreen(s);
-        inventory.addMouse(m);
+        inventory.addItem(samsungScreen, 5);
 
-        for(int i=0; i < inventory.screens.length; i++ ) {
-            if(inventory.screens[i] != null) {
-                System.out.println(inventory.screens[i].brand);
-            }
-        }
+        inventory.addItem(dellMouse, 12);
+        inventory.removeItem(dellMouse, 2);
+        inventory.displayInventoryOnConsole();
 
-        for(int j = 0; j < inventory.mice.length; j++ ) {
-            if(inventory.mice[j] != null) {
-                System.out.println(inventory.mice[j].brand);
-            }
-        }
+        inventory.removeItem(dellMouse, 12);
 
-
-    }
-}
+        inventory.displayInventoryOnConsole();
+        inventory.displayItemsOnConsole();
+ }
+/**
+ * System.out.println("Inventory POC");
+ * 
+ * Mouse m = new Mouse("DELL", 20.0); Screen s = new Screen("Samsung", 150.0,
+ * "1920x1080");
+ * 
+ * Inventory inventory = new Inventory(); inventory.addScreen(s);
+ * inventory.addMouse(m);
+ * 
+ * for(int i=0; i < inventory.screens.length; i++ ) { if(inventory.screens[i] !=
+ * null) { System.out.println(inventory.screens[i].brand); } }
+ * 
+ * for(int j = 0; j < inventory.mice.length; j++ ) { if(inventory.mice[j] !=
+ * null) { System.out.println(inventory.mice[j].brand); } }
+ * 
+ * 
+ * }
+ */
